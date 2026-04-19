9ac4387e87de5b6213bd8bb6d31cdb79850e6852
moalimirinfinity <moalimirinfinity@gmail.com>
2026-04-19 22:22:32 +0330

    tests removed for refactoring

--- tests/test_operations.py
+++
@@ -1,9 +0,0 @@
-from medium_stealth_bot.operations import USER_FOLLOWERS_MAX_LIMIT, user_followers
-
-
-def test_user_followers_clamps_limit_to_supported_range() -> None:
-    high = user_followers(user_id="abc123", limit=200)
-    low = user_followers(user_id="abc123", limit=0)
-
-    assert high.variables["paging"]["limit"] == USER_FOLLOWERS_MAX_LIMIT
-    assert low.variables["paging"]["limit"] == 1
