0bfbe18b4993d5c223d8543c72aa1423036744ed
moalimirinfinity <moalimirinfinity@gmail.com>
2026-04-21 12:19:08 +0330

    chore: update src/medium_stealth_bot/database.py

--- src/medium_stealth_bot/database.py
+++ src/medium_stealth_bot/database.py
@@ -19,7 +19,7 @@ class SqlMigration:
 class Database:
     """SQLite wrapper with file-based numbered SQL migrations."""
 
-    TARGET_SCHEMA_VERSION = 6
+    TARGET_SCHEMA_VERSION = 9
 
     def __init__(self, path: Path):
         self.path = path
